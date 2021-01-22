b7d4a8967231d582c832e3dbc33f819d80b6e894
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-22 18:57:46 -0500

    added dependencies and built the basic connection in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "LS2jNHzXtr[LQB&+4I#y",
+
+    database: "business_DB",
+  });
+
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    // init();
+  });
+
+  
