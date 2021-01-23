ab6455b8804db230a4b2952d2321668aade25b91
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-22 22:11:38 -0500

    built addEmployee function

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ connection.connect(function (err) {
 });
 
 function viewEmployees() {
-  queryString = `SELECT CONCAT(employee.first_name," ", employee.last_name) AS "Employee", role.title AS "Job", role.salary AS "Salary", department.name AS "Dept.", IFNULL(CONCAT(mgmt.first_name," ",mgmt.last_name),"Department Lead") AS "Manager Name"
+  const queryString = `SELECT CONCAT(employee.first_name," ", employee.last_name) AS "Employee", role.title AS "Job", role.salary AS "Salary", department.name AS "Dept.", IFNULL(CONCAT(mgmt.first_name," ",mgmt.last_name),"Department Lead") AS "Manager Name"
   FROM employee
   INNER JOIN role
   ON role.id = employee.role_id
@@ -37,7 +37,7 @@ function viewEmployees() {
 }
 
 function viewRoles() {
-  queryString = `
+  const queryString = `
   SELECT role.title AS "Job Title", role.salary AS "Salary", department.name AS "Dept."
   FROM role
   INNER JOIN department
@@ -50,7 +50,7 @@ function viewRoles() {
 };
 
 function viewDepartments() {
-  queryString = `
+  const queryString = `
   SELECT name AS "Department Name"
   FROM department;`;
   connection.query(queryString, (err, data) => {
@@ -60,7 +60,58 @@ function viewDepartments() {
   });
 };
 
-
+function addEmployee() {
+  const managerQuery = `SELECT CONCAT(first_name, " ", last_name) AS managers, id FROM employee;`;
+  const roleQuery = `SELECT title, id FROM role;`;
+  const managers = [];
+  const roles = [];
+  connection.query(managerQuery, (err, data) => {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++){
+      managers.push({name: data[i].managers, value: data[i].id});
+    };
+    managers.push({name: 'None', value: 0});
+    // managers.push(data.managers);
+    console.log(managers);
+  });
+  connection.query(roleQuery, (err, data) => {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++){
+      roles.push({name: data[i].title, value: data[i].id});
+    };
+    console.log(roles);
+  })
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is this employee's first name?",
+      name: "first_name",
+    },
+    {
+      type: "input",
+      message: "What is this employee's last name?",
+      name: "last_name",
+    },
+    {
+      type: "list",
+      message: "What is this employee's job title?",
+      choices: roles,
+      name: "role",
+    },
+    {
+      type: "list",
+      message: "Who is this employee's manager?",
+      choices: managers,
+      name: "manager",
+    },
+  ]).then((res) => {
+    const queryString = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`;
+    connection.query(queryString, [res.first_name, res.last_name, res.role, res.manager], (err, data) => {
+      if (err) throw err;
+      init();
+    });
+  });
+};
 
 function exitApp() {
   console.log("See ya later!");
