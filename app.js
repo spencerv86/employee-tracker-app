fbaebfd3907e5a376c35172e931754a2d3c0343a
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-22 23:28:07 -0500

    built updateEmployee function

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function viewRoles() {
     console.table(data);
     init();
   });
-};
+}
 
 function viewDepartments() {
   const queryString = `
@@ -58,7 +58,7 @@ function viewDepartments() {
     console.table(data);
     init();
   });
-};
+}
 
 function addEmployee() {
   const managerQuery = `SELECT CONCAT(first_name, " ", last_name) AS managers, id FROM employee;`;
@@ -67,112 +67,174 @@ function addEmployee() {
   const roles = [];
   connection.query(managerQuery, (err, data) => {
     if (err) throw err;
-    for (let i = 0; i < data.length; i++){
-      managers.push({name: data[i].managers, value: data[i].id});
-    };
-    managers.push({name: 'None', value: 0});
+    for (let i = 0; i < data.length; i++) {
+      managers.push({ name: data[i].managers, value: data[i].id });
+    }
+    managers.push({ name: "None", value: 0 });
     // managers.push(data.managers);
     // console.log(managers);
   });
   connection.query(roleQuery, (err, data) => {
     if (err) throw err;
-    for (let i = 0; i < data.length; i++){
-      roles.push({name: data[i].title, value: data[i].id});
-    };
+    for (let i = 0; i < data.length; i++) {
+      roles.push({ name: data[i].title, value: data[i].id });
+    }
     // console.log(roles);
-  })
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is this employee's first name?",
-      name: "first_name",
-    },
-    {
-      type: "input",
-      message: "What is this employee's last name?",
-      name: "last_name",
-    },
-    {
-      type: "list",
-      message: "What is this employee's job title?",
-      choices: roles,
-      name: "role",
-    },
-    {
-      type: "list",
-      message: "Who is this employee's manager?",
-      choices: managers,
-      name: "manager",
-    },
-  ]).then((res) => {
-    const queryString = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`;
-    connection.query(queryString, [res.first_name, res.last_name, res.role, res.manager], (err, data) => {
-      if (err) throw err;
-      console.log(`${res.first_name} ${res.last_name} has been added as a new ${res.role}.`)
-      init();
-    });
   });
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is this employee's first name?",
+        name: "first_name",
+      },
+      {
+        type: "input",
+        message: "What is this employee's last name?",
+        name: "last_name",
+      },
+      {
+        type: "list",
+        message: "What is this employee's job title?",
+        choices: roles,
+        name: "role",
+      },
+      {
+        type: "list",
+        message: "Who is this employee's manager?",
+        choices: managers,
+        name: "manager",
+      },
+    ])
+    .then((res) => {
+      const queryString = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);`;
+      connection.query(
+        queryString,
+        [res.first_name, res.last_name, res.role, res.manager],
+        (err, data) => {
+          if (err) throw err;
+          console.log(
+            `${res.first_name} ${res.last_name} has been added as a new ${res.role}.`
+          );
+          init();
+        }
+      );
+    });
+}
 
 function addRole() {
   const deptQuery = `SELECT name AS "departments", id FROM department;`;
   const departments = [];
   connection.query(deptQuery, (err, data) => {
-    for (let i = 0; i < data.length; i++){
-      departments.push({name: data[i].departments, value: data[i].id});
-    };
+    for (let i = 0; i < data.length; i++) {
+      departments.push({ name: data[i].departments, value: data[i].id });
+    }
     // console.log(departments);
   });
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is this role's title?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "What is this position's salary? (please enter number with no punctuation)",
-      name: "salary",
-      validation: function (input) {
-        if (/^\d+$/.test(input)){
-          return true;
-        } else {
-          return "That is an invalid salary.";
-        };
-      }
-    },
-    {
-      type: "list",
-      message: "What department will this role fall under?",
-      name: "dept_id",
-      choices: departments,
-    },
-  ]).then((res) => {
-    const queryString = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`;
-    connection.query(queryString, [res.title, res.salary, res.dept_id], (err) => {
-      if (err) throw err;
-      console.log(`${res.title} has been added as a new role.`);
-      init();
-    })
-  });
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is this role's title?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message:
+          "What is this position's salary? (please enter number with no punctuation)",
+        name: "salary",
+        validation: function (input) {
+          if (/^\d+$/.test(input)) {
+            return true;
+          } else {
+            return "That is an invalid salary.";
+          }
+        },
+      },
+      {
+        type: "list",
+        message: "What department will this role fall under?",
+        name: "dept_id",
+        choices: departments,
+      },
+    ])
+    .then((res) => {
+      const queryString = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);`;
+      connection.query(
+        queryString,
+        [res.title, res.salary, res.dept_id],
+        (err) => {
+          if (err) throw err;
+          console.log(`${res.title} has been added as a new role.`);
+          init();
+        }
+      );
+    });
+}
 
 function addDept() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is this department's name?",
-      name: "dept_name",
-    },
-  ]).then((res) => {
-    const queryString = `INSERT INTO department (name) VALUES (?);`;
-    connection.query(queryString, [res.dept_name], (err) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is this department's name?",
+        name: "dept_name",
+      },
+    ])
+    .then((res) => {
+      const queryString = `INSERT INTO department (name) VALUES (?);`;
+      connection.query(queryString, [res.dept_name], (err) => {
+        if (err) throw err;
+        console.log(`The ${res.dept_name} department has been added.`);
+        init();
+      });
+    });
+}
+
+function updateEmp() {
+  const employeeQuery = `SELECT CONCAT(first_name, " ", last_name) AS employees, id FROM employee;`;
+  const rolesQuery = `SELECT title, id FROM role;`;
+  let employees = [];
+  let roles = [];
+  connection.query(employeeQuery, (err, data) => {
+    if (err) throw err;
+    // console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      employees.push({ name: data[i].employees, value: data[i].id });
+    }
+    // console.log(employees);
+    connection.query(rolesQuery, (err, data) => {
       if (err) throw err;
-      console.log(`The ${res.dept_name} department has been added.`)
-      init();
-    })
+      for (let i = 0; i < data.length; i++) {
+        roles.push({ name: data[i].title, value: data[i].id });
+      }
+      // console.log(roles);
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "Which employee would you like to update?",
+            choices: employees,
+            name: "emp_id",
+          },
+          {
+            type: "list",
+            message: "What will their new role be?",
+            choices: roles,
+            name: "new_role",
+          },
+        ])
+        .then((res) => {
+          const queryString = `UPDATE employee SET role_id = ? WHERE id = ?;`;
+          connection.query(queryString, [res.new_role, res.emp_id], (err) => {
+            if (err) throw err;
+            console.log(`The employee's role has been updated.`);
+            init();
+          });
+        });
+    });
   });
-};
+}
 
 function exitApp() {
   console.log("See ya later!");
