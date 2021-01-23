1dbc8bdb369d227d10365fa3155a6d1b8c48623a
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-22 19:57:03 -0500

    added the initial inquirer prompt

--- app.js
+++ app.js
@@ -22,6 +22,64 @@ const connection = mysql.createConnection({
 
 
   function init() {
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "welcomePrompt",
+        choices: ["View All Employees",
+        "View All Roles",
+        "View All Departments", 
+        "View All Employees By Department", 
+        "View All Employees By Role", 
+        "Add Employee", 
+        "Update Employee Role", 
+        "Add Role", 
+        "Add Department",
+        "Exit Tracker"],
+      },
+    ]).then(({welcomePrompt}) => {
+      switch (welcomePrompt) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+
+        case "View All Roles":
+          viewRoles();
+          break;
+        
+        case "View All Departments":
+          viewDepartments();
+          break;
+
+        case "View All Employees By Department":
+          viewEmpDepts();
+          break;
+        
+        case "View All Employees By Role":
+          viewEmpRoles();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Department":
+          addDept();
+          break;
+
+        case "Update Employee Role":
+          updateEmp();
+          break;
+
+        default:
+          exitApp();
+      }
+    })
 
   };
   
